12dd128b1bbb5f0738c8e4149cd0d6ca781dbe82
yg1110 <younggil9488@gmail.com>
2025-03-22 00:50:49 +0900

    [fix] 이슈 상세 스타일 잘못된 부분 수정

--- src/routes/pages/issues/[id]/components/IssueBody.tsx
+++ src/routes/pages/issues/[id]/components/IssueBody.tsx
@@ -3,18 +3,17 @@ import ReactMarkdown from "react-markdown";
 import { GitHubUser } from "../../../../../schemas/github-issue";
 
 interface Props {
-  number: number;
   body: string | null;
   user: GitHubUser;
   created_at: string;
 }
-export default function IssueBody({ number, body, user, created_at }: Props) {
+export default function IssueBody({ body, user, created_at }: Props) {
   return (
     <div className="w-full md:w-[80%] order-2 md:order-1 whitespace-pre-line bg-white space-y-6">
       {body ? (
         <div className="bg-white rounded-md border border-gray-200 mb-6">
           <div className="flex items-start gap-3 bg-[#ddf4ff] p-2">
-            <a href={`/${user.login}`} className="shrink-0">
+            <a href={`https://github.com/${user.login}`} className="shrink-0">
               <img src={user.avatar_url} alt={`@${user.login}`} className="w-6 h-6 rounded-full" />
             </a>
             <div className="flex flex-col text-sm">
@@ -23,13 +22,13 @@ export default function IssueBody({ number, body, user, created_at }: Props) {
                   {user.login}
                 </a>
                 <span className="text-gray-500">opened</span>
-                <a href={`https://github.com/${user.login}/issues/${number}`} className="text-gray-500 hover:underline">
+                <p className="text-gray-500">
                   {new Date(created_at).toLocaleDateString(undefined, {
                     month: "short",
                     day: "numeric",
                     year: "numeric",
                   })}
-                </a>
+                </p>
               </div>
             </div>
           </div>
