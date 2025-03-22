030e4ca380c81f7cd68f8243d99deb5ed8a2ca31
yg1110 <younggil9488@gmail.com>
2025-03-22 20:31:24 +0900

    [feat] 이슈 상세에 수정 버튼 추가

--- src/routes/pages/issue/detail/components/IssueTitle.tsx
+++ src/routes/pages/issue/detail/components/IssueTitle.tsx
@@ -1,10 +1,25 @@
+import { useParams } from "react-router-dom";
+
+import Button from "@/shared/components/Button";
+import { usePageInfoWithHelmet } from "@/shared/hooks/usePageInfoWithHelmet";
+
 interface Props {
   title: string;
 }
 export default function IssueTitle({ title }: Props) {
+  const { id } = useParams<{ id: string }>();
+  const { user, repo } = usePageInfoWithHelmet();
   return (
-    <div className="mb-5 pb-3 border-b border-gray-300">
+    <div className="mb-5 pb-3 border-b border-gray-300 flex justify-between">
       <h2 className="text-xl font-semibold">{title}</h2>
+      <div className="flex gap-2">
+        <a href={`/${user}/${repo}/issue/${id}/edit`}>
+          <Button variant="outline">Edit</Button>
+        </a>
+        <a href={`/${user}/${repo}/issue/create`}>
+          <Button>New issue</Button>
+        </a>
+      </div>
     </div>
   );
 }
